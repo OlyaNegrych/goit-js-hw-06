543232fd303206a5c301aa43a32b181525f43be7
OlyaNegrych <olyanegrych@gmail.com>
2022-07-19 12:38:13 +0300

    completed task 7

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,6 @@ const inputRef = document.querySelector('#validation-input');
 
 const checkFn = () => {
     
-    console.log(inputRef.value.length);
-    console.log(Number(inputRef.dataset.length));
-    console.log(inputRef.value.length === Number(inputRef.dataset.length));
-
     if (inputRef.value.length !== Number(inputRef.dataset.length)) {
       inputRef.classList.add('invalid');
     } else {
---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const inputRef = document.querySelector('#font-size-control');
+const textRef = document.querySelector('#text');
+
+const changeFontSize = (event) => {
+
+    textRef.style.fontSize = `${event.currentTarget.value}px`;
+};
+
+inputRef.addEventListener('input', changeFontSize);
