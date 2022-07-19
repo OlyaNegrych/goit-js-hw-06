20f49f2675fca6665b5cdd77a960e1ab09690b24
OlyaNegrych <olyanegrych@gmail.com>
2022-07-19 17:25:39 +0300

    completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const colorMix = Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0);
+  return `#${colorMix}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const textRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+const changeColor = () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  textRef.textContent = getRandomHexColor();
+}
+
+btnRef.addEventListener('click', changeColor);
+
