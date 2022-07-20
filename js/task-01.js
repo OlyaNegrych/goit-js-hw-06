5f5387f0e1b77df055d5c5a2bb3265906c7a7320
OlyaNegrych <olyanegrych@gmail.com>
2022-07-20 23:23:01 +0300

    fixed task 1

--- js/task-01.js
+++ js/task-01.js
@@ -5,42 +5,28 @@ console.log(`Number of categories: ${numberOfCategories.length}`);
 
 //-------------Завдання 1.2.---------------------
 
-const categoryAllitems = document.querySelectorAll('.item');
-const categoryTitle = document.querySelectorAll('h2');
-const categoryLi = document.querySelectorAll('.item li');
+const categoriesTitles = document.querySelectorAll('h2');
 
-console.log(`Category: ${categoryTitle[0].textContent}`);
-console.log(`Elements: ${categoryLi[0].length}`);
+const categoryFirstItem = document
+    .querySelectorAll('.item')[0]
+    .querySelectorAll('li');
 
-console.log(`Category: ${categoryTitle[1].textContent}`);
-console.log(`Elements: ${categoryLi[0].length}`);
+console.log(`Category: ${categoriesTitles[0].textContent}`);
+console.log(`Elements: ${categoryFirstItem.length}`);
 
-console.log(`Category: ${categoryTitle[2].textContent}`);
-console.log(`Elements: ${categoryLi[0].length}`);
+const categorySecondItem = document
+    .querySelectorAll('.item')[1]
+    .querySelectorAll('li');
 
+console.log(`Category: ${categoriesTitles[1].textContent}`);
+console.log(`Elements: ${categorySecondItem.length}`);
 
-// const categoriesTitles = document.querySelectorAll('h2');
+const categoryThirdItem = document
+    .querySelectorAll('.item')[2]
+    .querySelectorAll('li');
 
-// const categoryFirstItem = document
-//     .querySelectorAll('.item')[0]
-//     .querySelectorAll('li');
-
-// console.log(`Category: ${categoriesTitles[0].textContent}`);
-// console.log(`Elements: ${categoryFirstItem.length}`);
-
-// const categorySecondItem = document
-//     .querySelectorAll('.item')[1]
-//     .querySelectorAll('li');
-
-// console.log(`Category: ${categoriesTitles[1].textContent}`);
-// console.log(`Elements: ${categorySecondItem.length}`);
-
-// const categoryThirdItem = document
-//     .querySelectorAll('.item')[2]
-//     .querySelectorAll('li');
-
-// console.log(`Category: ${categoriesTitles[2].textContent}`);
-// console.log(`Elements: ${categoryThirdItem.length}`);
+console.log(`Category: ${categoriesTitles[2].textContent}`);
+console.log(`Elements: ${categoryThirdItem.length}`);
 
 
  
